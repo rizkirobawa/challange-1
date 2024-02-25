0750057bf99efbb73c3e8dd4c296971c0f828841
rizkirobawa <rizkirobawa@gmail.com>
2024-02-25 23:20:42 +0700

    first commit

---
+++ bank_account.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let saldo = 0;
+
+function tampilSaldo() {
+  document.getElementById("nilaiSaldo").textContent = saldo;
+}
+
+function tambahSaldo() {
+  // Meminta pengguna memasukkan jumlah saldo yang ingin ditambahkan
+  let tambahUang = parseInt(
+    window.prompt("Masukkan jumlah saldo yang ingin ditambahkan: ")
+  );
+
+  // Validasi input pengguna
+  if (tambahUang <= 0) {
+    window.alert("Input tidak valid.");
+    return;
+  }
+
+  // Menambahkan jumlah saldo
+  saldo += tambahUang;
+
+  tampilSaldo(); // Memanggil fungsi tampilSaldo setelah saldo diperbarui
+}
+
+function kurangiSaldo() {
+  // Meminta pengguna memasukkan jumlah saldo yang ingin dikurangi
+  let kurangiUang = parseInt(
+    window.prompt("Masukkan jumlah saldo yang ingin dikurangi:")
+  );
+
+  // Validasi input pengguna
+  if (kurangiUang <= 0) {
+    window.alert("Input tidak valid.");
+    return;
+  }
+
+  let saldoSetelahPengurangan = saldo - kurangiUang;
+
+  if (saldoSetelahPengurangan < 0) {
+    window.alert("Saldo tidak mencukupi jumlah minimum saldo.");
+    return;
+  }
+
+  saldo = saldoSetelahPengurangan;
+  tampilSaldo(); // Memanggil fungsi tampilSaldo setelah saldo diperbarui
+}
+
+// Menambahkan event listener ke tombol-tombol
+document.getElementById("tambahButton").addEventListener("click", tambahSaldo);
+document.getElementById("kurangiButton").addEventListener("click", kurangiSaldo);
+
+tampilSaldo(); // Memanggil fungsi tampilSaldo untuk menampilkan saldo awal
